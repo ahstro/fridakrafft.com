147aee3141dae72b9b314263676609ac9c556c79
Anton Strömkvist <hi@ahst.ro>
2016-03-08 23:04:21 +0100

    Revert "Use cssnano to minify CSS"
    cssnano broke everything and I forgot to test
    This reverts commit b0fe14154c99022136b6214daba5ced0a4c438f4.

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting PostCSS"
-postcss --use postcss-cssnext --use cssnano --local-plugins -o lib/style.css src/style.css --watch &
+postcss --use postcss-cssnext --local-plugins -o lib/style.css src/style.css --watch &
 
 echo "Starting Babel"
 babel src/script.js -o lib/script.js --presets es2015 --no-babelrc --minified --watch &
