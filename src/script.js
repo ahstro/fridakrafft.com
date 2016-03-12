c2aaa335ce049fb87e7d25ab854bc15ad315828a
Anton Strömkvist <hi@ahst.ro>
2016-03-12 20:26:37 +0100

    Highlight active button

--- src/script.js
+++ src/script.js
@@ -5,20 +5,32 @@ function slideHeaderUp () {
   }
 }
 
+function setActiveButton (id) {
+  Array.from(document.getElementsByClassName('menu-item')).forEach(x => {
+    x.className = x.hash === `#${id}`
+      ? `${x.className} active`
+      : x.className.replace('active', '')
+  })
+}
+
 function setContent (id) {
   if (!id) return
   slideHeaderUp()
   document.getElementById('content').innerHTML = document.getElementById(id).innerHTML
 }
 
+function updateSite (id) {
+  setContent(id)
+  setActiveButton(id)
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   // Set state from url hash
-  const id = window.location.hash.substr(1)
-  setContent(id)
+  updateSite(window.location.hash.substr(1))
 
   // Set event listener to show content on click
   document.getElementById('nav').addEventListener('click', e => {
     if (e.target.tagName.toLowerCase() !== 'a') return
-    setContent(e.target.hash.substr(1))
+    updateSite(e.target.hash.substr(1))
   })
 })
